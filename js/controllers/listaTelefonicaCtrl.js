3615a61542757dfe06b5ab490b3f0f6a24071f98
MarcusGalli <marcus.galli@gmail.com>
2016-02-19 18:40:55 -0200

    Diretivas 1
    Criando um simples componente de msg de erro utilizando diretivas.
    .template e templateUrl> Especifica o template ou url do template que
    deverá ser incluído dentro do elemento que estiver utilizando a
    diretiva.
    
    .replace > Substitui o elemento pelo template da diretiva.
    
    .restrict > Restringe o modo de utilização da diretiva ao atributo,
    elemento, classe e comentário, ou ainda uma combinação deles. Caso não
    seja definido, o padrão é que a diretiva seja atribuída pelo atributo.
    Os tipos de restrição são:
    A - Diretiva restrita ao atributo do elemento
    E - Diretiva restrita ao elemento
    C - Diretiva restrita a classe do elemento
    M - Diretiva restrita ao comentário do elemento
    
    .scope > Por padrão, uma diretiva compartilha o mesmo scope de onde é
    utilizada. Para aumentar seu potencial de reuso, podemos isolar seu
    scope, passando os dados necessários por parâmetro.
    
    .transclude > Encapsula elementos dentro de uma diretiva, criando um
    scope não isolado.

--- js/controllers/listaTelefonicaCtrl.js
+++ js/controllers/listaTelefonicaCtrl.js
@@ -7,12 +7,14 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($sc
 		contatosAPI.getContatos().success(function (data) {
 			$scope.contatos = data;
 		}).error(function (data, status) {
-			$scope.message = "Aconteceu um problema: " + data;
+			$scope.error = "Aconteceu um problema: " + data;
 		});
 	};
 	var carregarOperadoras = function () {
 		operadorasAPI.getOperadoras().success(function (data) {
 			$scope.operadoras = data;
+		}).error(function (data, status) {
+			$scope.error = "Aconteceu um problema: " + data;
 		});
 	};
 
